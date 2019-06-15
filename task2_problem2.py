bf46e0c58a52158ade8135edd597019bccf5d3fa
Arvinth-s <immortalarvinths@gmail.com>
2019-06-15 07:25:55 +0530

    I used numpy to implement hacker mode

--- task2_problem2.py
+++ task2_problem2.py
@@ -1,20 +1,20 @@
 from time import sleep
-
-
-def find_max(s, L, R, v):
-    for i in range(L-1, R):
-        s[i] +=v
-
+import numpy as np
 
 if __name__ == "__main__":
-    size = long long(raw_input("type the number of elements in the array:"))
-    n = long long(raw_input("type the number of queries:"))
+    size = long(raw_input())
+    n = long(raw_input())
     s = []
+    t = [1]*size
+    z = [0]*size
     for i in range(size):
         s.append(i+1)
+    a = np.array(s)
     for i in range(n):
-         s1 = raw_input("your query here:").rstrip().split()
+         s1 = raw_input().rstrip().split()
          L, R, v = s1
-         find_max(s, int(L), int(R), int(v))
+         L, R, v =int(L), int(R), int(v)
+         a+= np.array(z[:L-1]+t[L-1:R]+z[R:])*v
+    s = list(a)
     print max(s)
-    sleep(10)
+    sleep(5)
